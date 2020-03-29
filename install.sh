6687aec1712ccb9ea575ca377ff23c57fed7b17f
joo <yukota.yuki@hotmail.com>
2020-03-29 15:41:10 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -10,13 +10,37 @@
 python_ver=3.8.2
 nodejs_ver=13.12.0
 
-pyenv () {
+os_app_install () {
+    # https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+    # VMは別scriptに切り出して最初に実行する
+    apt install -y \
+        make \
+        build-essential \
+        libbz2-dev \
+        libreadline-dev \
+        libffi-dev \
+        liblzma-dev \
+        libsqlite3-dev \
+        libssl-dev \
+        zlib1g-dev \
+        wget \
+        curl \
+        llvm \
+        libncurses5-dev \
+        xz-utils \
+        tk-dev \
+        libxml2-dev \
+        libxmlsec1-dev 
+}
+
+pyenv_install () {
     git clone https://github.com/pyenv/pyenv.git ~/.pyenv
     echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
     echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
     echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-    #source ~/.bashrc
+}
 
+python_install () {
     export PYENV_ROOT="$HOME/.pyenv"
     export PATH="$PYENV_ROOT/bin:$PATH"
     export PATH="$PYENV_ROOT/shims:$PATH"
@@ -38,41 +62,19 @@ pyenv () {
       esac
     }
 
-
-    # https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-    # ここはsudo権限がいるので、dockerだとRUNで実施しておく
-    # VMは別scriptに切り出して最初に実行する
-    apt install -y \
-        make \
-        build-essential \
-        libbz2-dev \
-        libreadline-dev \
-        libffi-dev \
-        liblzma-dev \
-        libsqlite3-dev \
-        libssl-dev \
-        zlib1g-dev \
-        wget \
-        curl \
-        llvm \
-        libncurses5-dev \
-        xz-utils \
-        tk-dev \
-        libxml2-dev \
-        libxmlsec1-dev 
-
     pyenv install $python_ver
     pyenv rehash
     pyenv global $python_ver
 }
 
-nodenv () {
+nodenv_install () {
     git clone git://github.com/nodenv/nodenv.git ~/.nodenv
     git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins/node-build
     echo 'export PATH="$HOME/.nodenv/bin:$PATH"' >> ~/.bashrc
     echo 'eval "$(nodenv init -)"' >> ~/.bashrc
-    #source ~/.bashrc
+}
 
+nodejs_install () {
     export PATH="$HOME/.nodenv/bin:$PATH"
     export PATH="$HOME/.nodenv/shims:$PATH"
     export NODENV_SHELL=bash
@@ -98,6 +100,9 @@ nodenv () {
     nodenv global $nodejs_ver
 }
 
-pyenv
-nodenv
+os_app_install
+pyenv_install
+python_install
+nodenv_install
+nodejs_install
 
