a03f991e8cd0979d142c9f870526f6e3605fd6f9
joo <yukota.yuki@hotmail.com>
2020-03-30 10:16:01 +0000

    update setup.sh for vim support

--- setup.sh
+++ setup.sh
@@ -10,8 +10,23 @@ dotfiles () {
         rm -rf $HOME/.config
     fi
 
+    if [ -e "$HOME/.vimrc" ]; then
+        rm -rf $HOME/.vimrc
+    fi
+
+    if [ -e "$HOME/.vim" ]; then
+        rm -rf $HOME/.vim
+    fi
+
+
     echo "$HOME/.dotfiles/config -> $HOME/.config"
     ln -sf $HOME/.dotfiles/config $HOME/.config
+
+    echo "$HOME/.dotfiles/config/nvim/init.vim -> $HOME/.vimrc"
+    ln -sf $HOME/.dotfiles/config/nvim/init.vim $HOME/.vimrc
+
+    echo "$HOME/.dotfiles/config/nvim -> $HOME/.vim"
+    ln -sf $HOME/.dotfiles/config/nvim $HOME/.vim
 }
 
 
