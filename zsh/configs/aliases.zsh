92192f71de8ec6329e02392c3aa7b54113074a2c
joo <yukota.yuki@hotmail.com>
2021-01-21 14:23:25 +0900

    zshの設定更新

--- zsh/configs/aliases.zsh
+++ zsh/configs/aliases.zsh
@@ -4,18 +4,16 @@
 #
 ###################################################
 
-#alias ls="ls -G"
-alias ls="lsd"
+alias ls="ls -G"
+#alias ls="lsd"
 alias l='ls'
 alias vim='nvim'
 alias ll='ls -l'
 alias la='ls -a'
-alias fgo='fg'
-alias cdt='cd $(date "+%Y_%m_%d")'
 alias weather='curl http://wttr.in/'
-alias pyinstaller='pyinstaller --additional-hooks-dir ./'
-#alias pyinstaller='pyinstaller --onefile --additional-hooks-dir ./'
-alias pyins_hooks_list='ls ~/.pyenv/versions/3.7.3/lib/python3.7/site-packages/PyInstaller/hooks/'
 #alias search_file='find * -type f -print | xargs grep '
-alias aws_gip="aws ec2 describe-instances | jq '.Reservations[].Instances[].PublicIpAddress'"
 alias tsource="tmux source"
+
+if [[ $GIT_EDITOR == 'nvr' ]]; then
+    alias vim = 'nvr'
+fi
--- zsh/configs/anyenv.zsh
+++ zsh/configs/anyenv.zsh
@@ -2,15 +2,18 @@
 #export PATH="$HOME/.jenv/bin:$PATH"
 #eval "$(jenv init -)"
 
+export JAVA_HOME=`/usr/libexec/java_home -v 11.0.8`
+
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/shims:$PATH"
+export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
-export GOENV_ROOT="$HOME/.goenv"
-export PATH="$GOENV_ROOT/bin:$PATH"
-export GOPATH="$HOME/go"
-export PATH="$HOME/go/bin:$PATH"
-eval "$(goenv init -)"
+#export GOENV_ROOT="$HOME/.goenv"
+#export PATH="$GOENV_ROOT/bin:$PATH"
+#export GOPATH="$HOME/go"
+#export PATH="$HOME/go/bin:$PATH"
+#eval "$(goenv init -)"
 
 
 #export PATH="$HOME/.ndenv/shims:$PATH"
@@ -19,3 +22,6 @@ export PATH="$HOME/.nodenv/bin:$PATH"
 eval "$(nodenv init -)"
 
 export PATH="$HOME/.local/bin:$PATH"
+
+# nim
+export PATH="$HOME/.nimble/bin:$PATH"
