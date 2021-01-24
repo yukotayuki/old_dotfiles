5564697c38e9ee5a1d2f06c342da63ec265e4b1e
joo <yukota.yuki@hotmail.com>
2021-01-24 16:44:34 +0900

    zinitに対応した設定に変更

--- config/zsh/configs/anyenv.zsh
+++ config/zsh/configs/anyenv.zsh
@@ -1,18 +1,18 @@
-# pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+## pyenv
+#export PYENV_ROOT="$HOME/.pyenv"
+#export PATH="$PYENV_ROOT/bin:$PATH"
+#eval "$(pyenv init -)"
 
-export GOENV_ROOT="$HOME/.goenv"
-export PATH="$GOENV_ROOT/bin:$PATH"
-export GOPATH="$HOME/go"
-export PATH="$HOME/go/1.14.2/bin:$PATH"
-eval "$(goenv init -)"
+#export GOENV_ROOT="$HOME/.goenv"
+#export PATH="$GOENV_ROOT/bin:$PATH"
+#export GOPATH="$HOME/go"
+#export PATH="$HOME/go/1.14.2/bin:$PATH"
+#eval "$(goenv init -)"
 
 # nodenv
 export PATH="$HOME/.nodenv/bin:$PATH"
 eval "$(nodenv init -)"
 
-# nim
-export PATH="$HOME/.nimble/bin:$PATH"
-# export PATH="$HOME/.local/bin:$PATH"
+## nim
+#export PATH="$HOME/.nimble/bin:$PATH"
+## export PATH="$HOME/.local/bin:$PATH"
